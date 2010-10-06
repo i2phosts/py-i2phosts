e21d608b323c691ac44c90c5287db78ccc3789d9
Hidden Z <hiddenz@mail.i2p>
2010-10-06 19:02:08 +0000

    web/postkey/models.py: update b4hash description

--- web/postkey/models.py
+++ web/postkey/models.py
@@ -4,6 +4,6 @@ class i2phost(models.Model):
 	# Hostname limit is 67 characters maximum, including the '.i2p'.
 	name = models.CharField("I2P hostname", max_length=67)
 	# Maximum key length 616 bytes (to account for certs up to 100 bytes).
-	b64hash = models.CharField("b64 hash", max_length=616)
+	b64hash = models.CharField("Base 64 hash", max_length=616)
 	date_added = models.DateTimeField(auto_now_add=True)
 	activated = models.BooleanField(default=False)
