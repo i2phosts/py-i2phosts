08ffe2942eecb9bc0ff240ccbfa6be0b7d6b3ec0
Hidden Z <hiddenz@mail.i2p>
2010-10-06 19:02:08 +0000

    web/postkey/models.py: make "name" and "b64hash" fields unique

--- web/postkey/models.py
+++ web/postkey/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class i2phost(models.Model):
 	# Hostname limit is 67 characters maximum, including the '.i2p'.
-	name = models.CharField("I2P hostname", max_length=67)
+	name = models.CharField("I2P hostname", max_length=67, unique=True)
 	# Maximum key length 616 bytes (to account for certs up to 100 bytes).
-	b64hash = models.CharField("Base 64 hash", max_length=616)
+	b64hash = models.CharField("Base 64 hash", max_length=616, unique=True)
 	date_added = models.DateTimeField(auto_now_add=True)
 	activated = models.BooleanField(default=False)
