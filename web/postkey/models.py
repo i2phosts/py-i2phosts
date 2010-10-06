662399433d23c242636ea71c7721916e368b1a13
Hidden Z <hiddenz@mail.i2p>
2010-10-06 19:02:08 +0000

    web/postkey/models.py: add "description" field

--- web/postkey/models.py
+++ web/postkey/models.py
@@ -5,5 +5,6 @@ class i2phost(models.Model):
 	name = models.CharField("I2P hostname", max_length=67, unique=True)
 	# Maximum key length 616 bytes (to account for certs up to 100 bytes).
 	b64hash = models.CharField("Base 64 hash", max_length=616, unique=True)
+	description = models.TextField("Description", blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
 	activated = models.BooleanField(default=False)
