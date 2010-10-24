d6f9f2ed995f20a07fe8c1aa013d09da120f429c
Hidden Z <hiddenz@mail.i2p>
2010-10-24 07:13:22 +0000

    web/jump/urls.py: modify regexp for jumper
    Now user can juery /jump/foo.i2p/anything and it will not produce
    "unknown hostname" error, instead user will be redirected to
    http://foo.i2p?i2paddresshelper=key

--- web/jump/urls.py
+++ web/jump/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('web.jump.views',
 		(r'^error/', 'error'),
 		(r'^unknown/', 'unknown'),
-		(r'^(.+)', 'jumper'),
+		(r'^([^$/]+)', 'jumper'),
 
 )
