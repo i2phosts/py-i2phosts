bd7417f9002e5589e0137cc498a92279973e8139
Hidden Z <hiddenz@mail.i2p>
2010-11-05 21:21:16 +0000

    Implement index view throught django generic views

--- pyi2phosts/urls.py
+++ pyi2phosts/urls.py
@@ -5,16 +5,27 @@ from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 admin.autodiscover()
 
+from pyi2phosts.lib.utils import get_b32
 from pyi2phosts.extsources.models import ExternalSource
-from pyi2phosts.other.views import *
+import settings
 
 urlpatterns = patterns('',
-		url(r'^$', index, name='index'),
+		url(r'^$', direct_to_template, {
+			'template': 'index.html',
+			'extra_context': {
+				'title': settings.SITE_NAME,
+				'domain': settings.DOMAIN,
+				'b64': settings.MY_B64,
+				'b32': get_b32(settings.MY_B64)
+				}
+			}, name='index'),
 		url(r'^faq/$', direct_to_template, {
 			'template': 'faq.html',
-			'extra_context': {'title': settings.SITE_NAME,
-				'sources': ExternalSource.objects.filter(active=True)}
-			}, name='faq' ),
+			'extra_context': {
+				'title': settings.SITE_NAME,
+				'sources': ExternalSource.objects.filter(active=True)
+				}
+			}, name='faq'),
 		(r'^postkey/', include('pyi2phosts.postkey.urls')),
 		(r'^jump/', include('pyi2phosts.jump.urls')),
     # Example:
