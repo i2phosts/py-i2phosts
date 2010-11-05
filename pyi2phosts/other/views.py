866c451a67611cceae4b671516fb21a72859f673
Hidden Z <hiddenz@mail.i2p>
2010-11-05 14:38:38 +0000

    pyi2phosts/other/views.py: pass b32, b64 variables to template

--- pyi2phosts/other/views.py
+++ pyi2phosts/other/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render_to_response
+
+from pyi2phosts.lib.utils import get_b32
+
 import settings
 
 def index(request):
 	return render_to_response('index.html', {
 		'title': settings.SITE_NAME,
 		'domain': settings.DOMAIN,
+		'b64': settings.MY_B64,
+		'b32': get_b32(settings.MY_B64),
 		})
