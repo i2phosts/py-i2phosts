ba5ae8decca597141c4d9ba891a7a462322c1852
Hidden Z <hiddenz@mail.i2p>
2011-02-16 19:01:02 +0000

    pyi2phosts/latest/urls.py: enable rss feed for latest hosts

--- pyi2phosts/latest/urls.py
+++ pyi2phosts/latest/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls.defaults import *
+from pyi2phosts.lib.rss import LatestHostsFeed
 
 urlpatterns = patterns('pyi2phosts.latest.views',
 		url(r'^$', 'latest', name='latest'),
+		url(r'^rss/$', LatestHostsFeed()),
+
 )
