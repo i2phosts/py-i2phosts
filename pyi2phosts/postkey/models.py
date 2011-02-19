f81e9664f30f4e1fa7257afcb41a4c14d3907b57
Hidden Z <hiddenz@mail.i2p>
2011-02-19 14:58:23 +0000

    pyi2phosts/postkey/models.py: add i18n

--- pyi2phosts/postkey/models.py
+++ pyi2phosts/postkey/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 class i2phost(models.Model):
 	# Hostname limit is 67 characters maximum, including the '.i2p'.
-	name = models.CharField("I2P hostname", max_length=67, unique=True)
+	name = models.CharField(_('I2P hostname'), max_length=67, unique=True)
 	# Maximum key length 616 bytes (to account for certs up to 100 bytes).
-	b64hash = models.CharField("Base 64 hash", max_length=616)
-	description = models.CharField("Description", max_length=4096, blank=True)
+	b64hash = models.CharField(_('Base 64 hash'), max_length=616)
+	description = models.CharField(_('Description'), max_length=4096, blank=True)
 	date_added = models.DateTimeField(null=True, blank=True)
 	# Last time this host was up
 	last_seen = models.DateTimeField(null=True, blank=True)
