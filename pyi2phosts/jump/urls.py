69cc03237be5d16698b581fe2469fe229f216998
Hidden Z <hiddenz@mail.i2p>
2017-09-20 17:59:06 +0000

    Add names to jump views

--- pyi2phosts/jump/urls.py
+++ pyi2phosts/jump/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-        url(r'^([^$/]+)', views.jumper),
-        url(r'', views.index),
+        url(r'^([^$/]+)', views.jumper, name='jump-views-jumper'),
+        url(r'', views.index, name='jump-views-index'),
 ]
